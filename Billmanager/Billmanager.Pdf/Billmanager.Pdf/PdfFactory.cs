27c3c87d5fce44dfcddd8b3bef374b308c90174d
chrigi28 <christian.holinger@gmail.com>
2020-10-08 14:42:58 +0200

    multisided bills

--- Billmanager/Billmanager.Pdf/Billmanager.Pdf/PdfFactory.cs
+++ Billmanager/Billmanager.Pdf/Billmanager.Pdf/PdfFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace SkiaSharpSample.Samples
     public static class PdfFactory
     {
         private static float offsetTop = 180;
-        private static float offsetBottom = 300;
+        private static float offsetBottom = 100;
         private static float offsetLeft = 100;
         private static float offsetRight = 100;
         private static float pageWidth = 840;
@@ -26,19 +27,7 @@ namespace SkiaSharpSample.Samples
 
         public static void CreateBill(string path, BillDbt bill)
         {
-            var metadata = new SKDocumentPdfMetadata
-            {
-                Author = "Cool Developer",
-                Creation = DateTime.Now,
-                Creator = "Cool Developer Library",
-                Keywords = "SkiaSharp, Sample, PDF, Developer, Library",
-                Modified = DateTime.Now,
-                Producer = "SkiaSharp",
-                Subject = "SkiaSharp Sample PDF",
-                Title = "Sample PDF",
-            };
-
-            using var document = SKDocument.CreatePdf(path, metadata);
+            using var document = SKDocument.CreatePdf(path);
 
             if (document == null)
             {
@@ -60,28 +49,17 @@ namespace SkiaSharpSample.Samples
             };
 
             // draw page 1
-            using (var pdfCanvas = document.BeginPage(pageWidth, pageHeight))
+            using (var page1 = document.BeginPage(pageWidth, pageHeight))
             {
-                DrawLogo(pdfCanvas);
-                DrawCustomerAdress(pdfCanvas, defaultPaint, bill);
-                DrawHolderAdress(pdfCanvas, defaultPaint);
+                DrawLogo(page1);
+                DrawCustomerAdress(page1, defaultPaint, bill);
+                DrawHolderAdress(page1, defaultPaint);
 
-                DrawDetails(pdfCanvas, defaultPaint, bill);
+                DrawDetails(document, page1, defaultPaint, bill);
 
-                // draw contents
-                //pdfCanvas.DrawText("...PDF 1/2...", pageWidth / 2, pageHeight / 4, paint);
                 document.EndPage();
             }
-
-            // draw page 2
-            using (var pdfCanvas = document.BeginPage(pageWidth, pageHeight))
-            {
-                DrawLogo(pdfCanvas);
-                // draw contents
-                pdfCanvas.DrawText("...PDF 2/2...", pageWidth / 2, pageHeight / 4, defaultPaint);
-                document.EndPage();
-            }
-
+            
             // end the doc
             document.Close();
         }
@@ -130,13 +108,15 @@ namespace SkiaSharpSample.Samples
             pdfCanvas.DrawText($"{tel}", startRight, yStart, paint);
         }
 
-        private static void DrawDetails(SKCanvas pdfCanvas, SKPaint textPaint, BillDbt bill)
+        private static void DrawDetails(SKDocument document, SKCanvas currentPage, SKPaint textPaint, BillDbt bill)
         {
 
             #region Styles
+
+            var titelHeight = textHeight + textHeight / 3 + 3;
             using var boldTitel = new SKPaint
             {
-                TextSize = textHeight + textHeight / 3 + 3,
+                TextSize = titelHeight,
                 IsAntialias = true,
                 Color = SKColors.Black,
                 IsStroke = false,
@@ -161,17 +141,98 @@ namespace SkiaSharpSample.Samples
 
             #region BillDetails
             var yStart = startBill;
-            pdfCanvas.DrawText(Resources.Bill, offsetLeft, yStart, boldTitel);
-            yStart += 20;
-            pdfCanvas.DrawText($"{Resources.BillNumber}: {bill.BillNumber}", offsetLeft, yStart, textPaint);
+            currentPage.DrawText(Resources.Bill, offsetLeft, yStart, boldTitel);
+            yStart += titelHeight + 5;
+            currentPage.DrawText($"{Resources.BillNumber}: {bill.BillNumber}", offsetLeft, yStart, textPaint);
             yStart += rowSpacing;
-            pdfCanvas.DrawText($"{Resources.Date}: {bill.Date.ToShortDateString()}", offsetLeft, yStart, textPaint);
+            currentPage.DrawText($"{Resources.Date}: {bill.Date.ToShortDateString()}", offsetLeft, yStart, textPaint);
             yStart += rowSpacing;
             #endregion
             
             #region BillItemHeader
 
             yStart += 10;
+            float hStart;
+            DrawItemHeader(currentPage, textPaint, ref yStart, linePaint);
+
+            #endregion
+
+            #region BillItems
+
+            var pageCount = 1;
+
+            int pos = 0;
+            foreach (var item in bill.ItemPositions)
+            {
+                hStart = offsetLeft;
+                currentPage.DrawText($"{pos}", hStart, yStart, textPaint);
+                hStart += 50;
+                currentPage.DrawText($"{item.Description}", hStart, yStart, textPaint);
+                hStart += 350;
+                currentPage.DrawText($"{item.Amount}", hStart, yStart, textPaint);
+                hStart += 50;
+                currentPage.DrawText($"{item.Price}", hStart, yStart, textPaint);
+                hStart += 100;
+                currentPage.DrawText($"{item.Total}", hStart, yStart, textPaint);
+                pos++;
+                yStart += rowSpacing+5;
+
+                // end of page is close make a new one
+                if (pageHeight / 5 * 4 <= yStart)
+                {
+                    // todo add some page info  pagesize etc.
+                    WritePageNumber(currentPage, textPaint, pageCount);                    
+                    document.EndPage();
+                    currentPage = document.BeginPage(pageWidth, pageHeight);
+                    pageCount++;
+                    DrawLogo(currentPage);
+                    yStart = offsetTop;
+                    DrawItemHeader(currentPage, textPaint, ref yStart, linePaint);
+                }
+            }
+
+            if (pageCount > 1)
+            {
+                WritePageNumber(currentPage, textPaint, pageCount);   
+            }
+
+            #endregion
+
+            #region BillSummary
+            currentPage.DrawLine(offsetLeft, yStart, pageWidth - offsetRight, yStart, linePaint);
+            yStart += rowSpacing;
+            hStart = offsetLeft + 450; // begin price
+
+            var bruto = "Brutobetrag";
+            var currency = "CHF";
+            currentPage.DrawText($"{bruto}: ", hStart, yStart, textPaint);
+            hStart += 100; // width of price
+            currentPage.DrawText($"{bill.ItemPositions.Sum(f => f.Total)} {currency}", hStart, yStart, textPaint);
+
+            yStart += rowSpacing;
+            
+            hStart -= 100;
+            var fat = textPaint;
+            fat.FakeBoldText = true;
+            var netto = "Netto: ";
+            currentPage.DrawText($"{netto}", hStart, yStart, fat);
+            hStart += 100; // width of price
+            currentPage.DrawText($"{bill.NettoPrice} {currency}", hStart, yStart, fat);
+            yStart += rowSpacing;
+
+            #endregion
+
+            ////pdfCanvas.DrawText($"{bill.Customer.Address}", offsetLeft, offsetTop + logoheight + 30, textPaint);
+            ////pdfCanvas.DrawText($"{bill.Customer.Zip} {bill.Customer.Location}", offsetLeft, offsetTop + logoheight + 45, textPaint);
+        }
+
+        private static void WritePageNumber(SKCanvas currentPage, SKPaint textPaint, int pageCount)
+        {
+            currentPage.DrawText($"Seite {pageCount}", pageWidth - offsetRight - 50, pageHeight - offsetBottom, textPaint);
+        }
+
+        private static float DrawItemHeader(SKCanvas pdfCanvas, SKPaint textPaint, ref float yStart, SKPaint linePaint)
+        {
             pdfCanvas.DrawLine(offsetLeft, yStart, pageWidth - offsetRight, yStart, linePaint);
             yStart += rowSpacing;
 
@@ -186,61 +247,11 @@ namespace SkiaSharpSample.Samples
             pdfCanvas.DrawText($"Preis/Stk", hStart, yStart, textPaint);
             hStart += 100;
             pdfCanvas.DrawText($"Gesamt", hStart, yStart, textPaint);
-            
+
             yStart += 10;
             pdfCanvas.DrawLine(offsetLeft, yStart, pageWidth - offsetRight, yStart, linePaint);
             yStart += rowSpacing;
-            
-            #endregion
-
-            #region BillItems
-            
-            int pos = 0;
-            foreach (var item in bill.ItemPositions)
-            {
-                hStart = offsetLeft;
-                pdfCanvas.DrawText($"{pos}", hStart, yStart, textPaint);
-                hStart += 50;
-                pdfCanvas.DrawText($"{item.Description}", hStart, yStart, textPaint);
-                hStart += 350;
-                pdfCanvas.DrawText($"{item.Amount}", hStart, yStart, textPaint);
-                hStart += 50;
-                pdfCanvas.DrawText($"{item.Price}", hStart, yStart, textPaint);
-                hStart += 100;
-                pdfCanvas.DrawText($"{item.Total}", hStart, yStart, textPaint);
-
-                pos++;
-                yStart += rowSpacing+5;
-            }
-            
-            #endregion
-
-            #region BillSummary
-            pdfCanvas.DrawLine(offsetLeft, yStart, pageWidth - offsetRight, yStart, linePaint);
-            yStart += rowSpacing;
-            hStart = offsetLeft + 450; // begin price
-
-            var bruto = "Brutobetrag";
-            var currency = "CHF";
-            pdfCanvas.DrawText($"{bruto}: ", hStart, yStart, textPaint);
-            hStart += 100; // width of price
-            pdfCanvas.DrawText($"{bill.ItemPositions.Sum(f => f.Total)} {currency}", hStart, yStart, textPaint);
-
-            yStart += rowSpacing;
-            
-            hStart -= 100;
-            var fat = textPaint;
-            fat.FakeBoldText = true;
-            var netto = "Netto: ";
-            pdfCanvas.DrawText($"{netto}: ", hStart, yStart, fat);
-            hStart += 100; // width of price
-            pdfCanvas.DrawText($"{bill.NettoPrice} {currency}", hStart, yStart, fat);
-            yStart += rowSpacing;
-
-            #endregion
-
-            ////pdfCanvas.DrawText($"{bill.Customer.Address}", offsetLeft, offsetTop + logoheight + 30, textPaint);
-            ////pdfCanvas.DrawText($"{bill.Customer.Zip} {bill.Customer.Location}", offsetLeft, offsetTop + logoheight + 45, textPaint);
+            return yStart;
         }
     }
 }
